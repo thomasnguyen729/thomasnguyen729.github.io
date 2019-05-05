fc67668848c53e286812daa3eabb71322a3e565d
Thomas <thomasnguyen729@gmail.com>
2019-05-05 00:48:43 +0000

    updated init

--- emacs/init.el
+++ emacs/init.el
@@ -1,13 +1,49 @@
+
+;; standard
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
+
+;; appearance
+(setq visible-bell 1)
+(blink-cursor-mode 0)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+;; magit
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; org
+(setq org-agenda-files (quote ("~/org")))
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+
+
+;; cycle windows
+(global-set-key (kbd "C-c h")  'windmove-left)
+(global-set-key (kbd "C-c j")  'windmove-down)
+(global-set-key (kbd "C-c l") 'windmove-right)
+(global-set-key (kbd "C-c k")    'windmove-up)
+
+
+;; counsel swiper ivy
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (auctex magit))))
+ '(package-selected-packages (quote (auctex magit counsel org))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
